bd328131e72e20ee38f6762e1cc8318cafdcde10
Giocatory <nivolyas@gmail.com>
2021-08-01 08:35:59 +0300

    01.08.21

---
+++ String_Study/first_chars_in_line.py
@@ -0,0 +1,12 @@
+# Дополните приведенный код, используя срезы, так чтобы он вывел первые 12 символов строки s.
+# "In 2010, someone paid 10k Bitcoin for two pizzas."
+
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[:12])
+
+# Дополните приведенный код, используя срезы, так чтобы он вывел последние 9 символов строки s.
+# "In 2010, someone paid 10k Bitcoin for two pizzas."
+
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[-9:])
+
