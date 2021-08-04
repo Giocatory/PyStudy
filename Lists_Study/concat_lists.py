ef803decb30b67122d820962e9fd6c1cab1d0bff
Giocatory <nivolyas@gmail.com>
2021-08-04 10:39:41 +0300

    04.08.21

---
+++ Lists_Study/concat_lists.py
@@ -0,0 +1,6 @@
+print('[1, 2, 3, 1, 2, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 8, 9, 10, 11, 12, 13]')
+numbers1 = [1, 2, 3]
+numbers2 = [6]
+numbers3 = [7, 8, 9, 10, 11, 12, 13]
+nums = numbers1*2 + numbers2*9 + numbers3
+print(nums)
