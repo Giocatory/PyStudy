7a4f799d2f53e053245faab622730057074ccbf1
Giocatory <nivolyas@gmail.com>
2021-08-05 11:08:12 +0300

    05.08.21

--- Lists_Study/kth_letter_in_word.py
+++ Lists_Study/kth_letter_in_word.py
@@ -14,3 +14,14 @@
 # Если некоторые строки слишком короткие,
 # и в них нет символа с заданным номером, то такие строки при выводе нужно игнорировать.
 
+n = int(input())
+words_list = []
+for i in range(n):
+    words_list.append(input())
+k = int(input())
+for i in range(len(words_list)):
+    if len(words_list[i]) < k:
+        continue
+    else:
+        word = words_list[i]
+        print(word[k-1], end='')
