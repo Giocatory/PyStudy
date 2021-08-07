03bc7fd409024481913a592b3df44503247202a3
Giocatory <nivolyas@gmail.com>
2021-08-07 11:43:48 +0300

    07.08.21

--- experiment.py
+++ experiment.py
@@ -1,17 +1,9 @@
-n = int(input())
-l = []
-nl = []
-chl = []
-nulll = []
-for _ in range(n):
-    j = int(input())
-    if j < 0:
-        nl.append(j)
-    elif j == 0:
-        nulll.append(j)
-    else:
-        chl.append(j)
-l.extend(nl)
-l.extend(nulll)
-l.extend(chl)
-print(*l, sep='\n')
+s = input()
+ss = input()
+
+for i in range(len(s)):
+    if i == len(s)-1:
+        print(s[i])
+        break
+    print(s[i]+ss, end='')
+
