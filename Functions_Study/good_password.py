b1fc6c081e7e53e2357eea9c128403ebb195beda
Giocatory <nivolyas@gmail.com>
2021-08-11 15:58:03 +0300

    11.08.21

--- Functions_Study/good_password.py
+++ Functions_Study/good_password.py
@@ -10,7 +10,28 @@
 
 # объявление функции
 def is_password_good(password):
-    pass
+    numbers = [i for i in range(48, 58)]
+    count_nums = 0
+    small_letters = [i for i in range(97, 123)]
+    count_small = 0
+    big_letters = [i for i in range(65, 91)]
+    count_big = 0
+    for i in range(len(password)):
+        if ord(password[i]) in numbers:
+            count_nums += 1
+            break
+    for i in range(len(password)):
+        if ord(password[i]) in small_letters:
+            count_small += 1
+            break
+    for i in range(len(password)):
+        if ord(password[i]) in big_letters:
+            count_big += 1
+            break
+    if count_big != 0 and count_small != 0 and count_nums != 0 and len(password) >= 8:
+        return True
+    else:
+        return False
 
 
 # считываем данные
