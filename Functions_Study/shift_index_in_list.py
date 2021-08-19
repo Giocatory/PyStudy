7fc1d4d204c4c89bd9b686be67fa5cbd038be004
Giocatory <nivolyas@gmail.com>
2021-08-19 17:06:54 +0300

    19.08.21

---
+++ Functions_Study/shift_index_in_list.py
@@ -0,0 +1,13 @@
+def shift(lst, steps):
+    if steps < 0:
+        steps = abs(steps)
+        for i in range(steps):
+            lst.append(lst.pop(0))
+    else:
+        for i in range(steps):
+            lst.insert(0, lst.pop())
+
+
+nums = input().split()
+shift(nums, 1)
+print(*nums)
