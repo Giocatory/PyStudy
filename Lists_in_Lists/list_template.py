59337050061d80b01104304d3803fd4b1559c35c
Giocatory <nivolyas@gmail.com>
2021-08-24 15:22:27 +0300

    24.08.21

--- Lists_in_Lists/list_template.py
+++ Lists_in_Lists/list_template.py
@@ -1,10 +1,11 @@
 n = int(input())  # How many rows and columns will there be?
-lists = []
-for i in range(n):
-    new_list = []
-    for j in range(1, n+1):
-        new_list.append(j)
-    lists.append(new_list)
+lists = [[j for j in range(1, n+1)] for i in range(n)]
+# for i in range(n):
+#     new_list = []
+#     for j in range(1, n+1):
+#         new_list.append(j)
+#     lists.append(new_list)
 
 for rows in lists:
     print(rows)
+
