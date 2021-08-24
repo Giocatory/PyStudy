cbf170ac9918cef8f2eaf58812bd3ba235683501
Giocatory <nivolyas@gmail.com>
2021-08-24 15:44:30 +0300

    24.08.21

--- Lists_in_Lists/triangle_of_pascal.py
+++ Lists_in_Lists/triangle_of_pascal.py
@@ -11,3 +11,17 @@ for i in range(n):
     row = next_row(row)
     if len(row) == n:
         print(row)
+
+
+# def next_row(lines):
+#     lines = [1] + lines
+#     for i in range(1, len(lines) - 1):
+#         lines[i] += lines[i + 1]
+#     return lines
+#
+#
+# row = []
+# n = int(input())
+# for i in range(n):
+#     row = next_row(row)
+#     print(*row)
