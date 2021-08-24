813e9e26ad2ee3fa4909403096bbdeac22378d3d
Giocatory <nivolyas@gmail.com>
2021-08-24 09:19:30 +0300

    24.08.21

--- draft.py
+++ draft.py
@@ -2,4 +2,4 @@ nums = [int(i) for i in input().split()]
 for i in range(1, len(nums)):
     if i % 2 != 0:
         nums[i], nums[i-1] = nums[i-1], nums[i]
-print(*nums, sep=',')
+print(*nums, sep=' ')
