de07addf72453a0bd594a66a3182c5259f67b71e
Giocatory <nivolyas@gmail.com>
2021-08-28 05:14:41 +0300

    28.08.21

--- examples.py
+++ examples.py
@@ -1,14 +1,4 @@
-poets = [
-    ('Тургенев', 14),
-    ('Есенин', 13),
-    ('Маяковский', 28),
-    ('Фет', 15),
-    ('Лермонтов', 20)]
-
-for i in range(len(poets)):
-    for j in range(i+1, len(poets)):
-        if poets[i] > poets[j]:
-            poets[i], poets[j] = poets[j], poets[i]
-
-print(poets[0])
-print(poets[-1])
+fruits = {'apple', 'banana', 'cherry', 'avocado', 'pineapple', 'apricot', 'banana', 'avocado', 'grapefruit'}
+fruits = sorted(fruits, reverse=True)
+for i in fruits:
+    print(i)
