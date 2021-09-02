bb5d056f9bf0493bcdf1c03f20ec0459cf7bc06b
Giocatory <nivolyas@gmail.com>
2021-09-02 08:31:30 +0300

    02.09.21

---
+++ Dictionary_Study/common_word_in_text.py
@@ -0,0 +1,14 @@
+# код, чтобы он вывел наиболее часто встречающееся слово строки s.
+# Если таких слов несколько, должно быть выведено то, что меньше в лексикографическом порядке.
+
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange ' \
+    'barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot ' \
+    'currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon ' \
+    'pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple ' \
+    'barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit ' \
+    'banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate ' \
+    'barley '
+result = {i: s.count(i) for i in set(s.split())}
+max_count = max(result.values())
+res = {i: j for i, j in result.items() if j == max_count}
+print(sorted(res)[0])
