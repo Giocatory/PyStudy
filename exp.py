9e2e474c3c491aa374c6267871efa3e02e54f79f
Giocatory <nivolyas@gmail.com>
2021-09-05 23:35:43 +0800

    05.09.21

--- exp.py
+++ exp.py
@@ -1,34 +1,18 @@
-import random
-import string
+from fractions import Fraction as f
+from math import factorial as fack
 
-upper = set("ABCDEFGHJKLMNPQRSTUVWXYZ")
-digits = set(string.digits)
+n = int(input())
+list_results = []
+for i in range(1, n+1):
+    for j in range(n, 0, -1):
+        if i + j == n and i < j:
+            list_results.append(f"{i}/{j}")
+res = [f(i) for i in list_results]
+max_num = res[0].numerator  # числитель
+max_den = res[0].denominator  # знаменатель
+for i in res:
+    if i.numerator >= max_num and i.denominator >= max_den:
+        max_num = i.numerator
+        max_den = i.denominator
+print(f"{max_num}/{max_den}")
 
-
-def valid(s):
-    s = set(s)
-    invalid = s.difference(upper, digits)
-    both = s.intersection(upper) and s.intersection(digits)
-    return bool(both and not invalid)
-
-
-def generate_password(length):
-    pss = ''
-    while True:
-        all_letters = list("abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ") + list("23456789")
-        password = [random.choice(all_letters) for _ in range(length)]
-        for i in password:
-            pss += i
-        if not(pss.isupper() or pss.islower()or pss.isdigit()or pss.isalpha() or len(pss) == length):
-            break
-    print(pss)
-    pss = ''
-
-
-def generate_passwords(count, length):
-    for _ in range(count):
-        generate_password(length)
-
-
-n, m = int(input()), int(input())
-generate_passwords(n, m)
