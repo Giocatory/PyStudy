14983d6d17a02af9c4782b6c27a4ffc9559f743f
Giocatory <nivolyas@gmail.com>
2021-09-05 21:40:53 +0800

    05.09.21

--- exp.py
+++ exp.py
@@ -1,10 +1,28 @@
 import random
+import string
+
+upper = set("ABCDEFGHJKLMNPQRSTUVWXYZ")
+digits = set(string.digits)
+
+
+def valid(s):
+    s = set(s)
+    invalid = s.difference(upper, digits)
+    both = s.intersection(upper) and s.intersection(digits)
+    return bool(both and not invalid)
 
 
 def generate_password(length):
-    all_letters = list("abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ") + list("23456789")
-    password = [random.choice(all_letters) for _ in range(length)]
-    print(*password, sep='')
+    pss = ''
+    while True:
+        all_letters = list("abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ") + list("23456789")
+        password = [random.choice(all_letters) for _ in range(length)]
+        for i in password:
+            pss += i
+        if not(pss.isupper() or pss.islower()or pss.isdigit()or pss.isalpha() or len(pss) == length):
+            break
+    print(pss)
+    pss = ''
 
 
 def generate_passwords(count, length):
