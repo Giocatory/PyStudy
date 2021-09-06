d2720cb876cb109735ac21f59b998bf67ba2bf8a
Giocatory <nivolyas@gmail.com>
2021-09-06 15:18:48 +0800

    06.09.21

---
+++ Turtle_module.py
@@ -0,0 +1,4 @@
+import turtle
+for i in range(10,195,5):
+  turtle.left(90)
+  turtle.forward(i)
--- exp.py
+++ exp.py
@@ -1,18 +1,6 @@
-from fractions import Fraction as f
-from math import factorial as fack
+# print('Сопряженное число =', z.conjugate())
 
 n = int(input())
-list_results = []
-for i in range(1, n+1):
-    for j in range(n, 0, -1):
-        if i + j == n and i < j:
-            list_results.append(f"{i}/{j}")
-res = [f(i) for i in list_results]
-max_num = res[0].numerator  # числитель
-max_den = res[0].denominator  # знаменатель
-for i in res:
-    if i.numerator >= max_num and i.denominator >= max_den:
-        max_num = i.numerator
-        max_den = i.denominator
-print(f"{max_num}/{max_den}")
-
+z1, z2 = complex(input()), complex(input())
+res = z1**n + z2**n + (z1**n).conjugate() + (z2**(n+1)).conjugate()
+print(res)
