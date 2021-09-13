8957e113843f500aa896426bf2912def1fcf6444
Giocatory <nivolyas@gmail.com>
2021-09-13 20:46:11 +0800

    13.09.21

---
+++ work_with_files/big_lines.py
@@ -0,0 +1,15 @@
+# Вам доступен текстовый файл lines.txt, в котором записаны строки текста.
+# Напишите программу, которая выводит все строки наибольшей длины из файла,
+# не меняя их порядок.
+
+all_text = []
+with open('lines.txt') as file:
+    for line in file:
+        all_text.append(line.strip())
+all_text.sort(key=len, reverse=True)
+print(all_text[0])
+for i in range(1, len(all_text)):
+    if len(all_text[i]) >= len(all_text[0]):
+        print(all_text[i])
+    else:
+        break
