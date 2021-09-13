a3dfdd40516b8116bda807ae2331661a4ad1cb3c
Giocatory <nivolyas@gmail.com>
2021-09-13 13:55:29 +0800

    13.09.21

---
+++ work_with_files/sum_two_nums_from_file.py
@@ -0,0 +1,7 @@
+# Вам доступен текстовый файл nums.txt.
+# В файле записано два целых числа, они могут быть разделены символами пробела и конца строки.
+# Напишите программу, выводящую на экран сумму этих чисел.
+file = open('nums.txt')
+text_in_file = list(map(int, list(filter(lambda x: x != '', [line.strip() for line in file]))))
+print(sum(text_in_file))
+file.close()
