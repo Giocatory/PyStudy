0e0debbc3aa3fb0237bc5e5a26d645f0093b0ddd
Giocatory <nivolyas@gmail.com>
2021-09-13 13:02:57 +0800

    13.09.21

--- work_with_files/file_open_read_close.py
+++ work_with_files/file_open_read_close.py
@@ -2,3 +2,9 @@ file = open('example.txt')
 for line in file:
     print(line.strip())
 file.close()
+
+print()
+
+with(open('example.txt', 'r+')) as file:
+    for line in file:
+        print(line.strip())
